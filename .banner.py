1125f6a8b4b7c428edccca3ffc9ca9762d7a7f59
RohitSaxsena <Acc3ssDenied001@gmail.com>
2014-05-06 00:16:26 -0700

    Update and rename .gitignore to .banner.py

---
+++ .banner.py
@@ -0,0 +1,21 @@
+import urllib
+import optparse
+
+
+parser = optparse.OptionParser("usage : %prog "+\
+		"-u <Enter Taget URL Here>")
+parser.add_option('-u', dest = 'uname', type = 'string',\
+		help='Specify Target URL')
+(options, arg) = parser.parse_args()
+if (options.uname == None) :
+	print parser.usage
+	exit(0)
+else :
+	print '\t\t\t ######################################'
+	print '\t\t\t ##    Proudly Made By An INDIAN     ##'
+	print '\t\t\t ##   Author : Rohit Saxsena INDIA   ##'
+	print '\t\t\t ######################################'
+	uname = options.uname
+ 	url = urllib.urlopen(uname)
+for header , value in url.headers.items():
+	print header + ':' + value 	
